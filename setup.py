8d48374752bf0d3bda397160c390c416f57f9d71
Marc Evanstein <marc@marcevanstein.com>
2020-08-05 15:16:26 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymusicxml",
-    version="0.3.2",
+    version="0.3.3",
     author="Marc Evanstein",
     author_email="marc@marcevanstein.com",
     description="A simple python library for exporting MusicXML",
