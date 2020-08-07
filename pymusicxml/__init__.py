a0933d71002366bbbeeb102f819ea56959ae90d6
Marc Evanstein <marc@marcevanstein.com>
2020-08-06 17:45:41 -0700

    Added copyright notices to all files

--- pymusicxml/__init__.py
+++ pymusicxml/__init__.py
@@ -9,6 +9,23 @@ pitches, and tuplets are treated as containers for notes, chords, and rests.
 At the moment, this library is intended as a tool for composition, making it easier to construct and export MusicXML
 scores in Python. In the future, the ability to parse existing MusicXML scores may be added.
 """
+
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  #
+#  SCAMP (Suite for Computer-Assisted Music in Python)                                           #
+#  Copyright © 2020 Marc Evanstein <marc@marcevanstein.com>.                                     #
+#                                                                                                #
+#  This program is free software: you can redistribute it and/or modify it under the terms of    #
+#  the GNU General Public License as published by the Free Software Foundation, either version   #
+#  3 of the License, or (at your option) any later version.                                      #
+#                                                                                                #
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;     #
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     #
+#  See the GNU General Public License for more details.                                          #
+#                                                                                                #
+#  You should have received a copy of the GNU General Public License along with this program.    #
+#  If not, see <http://www.gnu.org/licenses/>.                                                   #
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  #
+
 from .music_xml_objects import Pitch, Duration, BarRestDuration, Note, Rest, BarRest, Chord, GraceNote, GraceChord, \
     BeamedGroup, Tuplet, Clef, Measure, Part, PartGroup, Score, Notehead, StartGliss, StopGliss, StartMultiGliss, \
     StopMultiGliss, StartSlur, StopSlur, MetronomeMark, TextAnnotation, EndDashedLine
